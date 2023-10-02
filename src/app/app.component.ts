fd1686db0f2aa4c761a99feeca8d1a793dc54030
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-10-02 03:02:00 +0300

    Finished Task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taskTwo';
+  username:string='';
+  isEmpty=false;
+
+
+  onReset(){
+    this.username="";
+    this.isEmpty=true
+  }
+
 }
